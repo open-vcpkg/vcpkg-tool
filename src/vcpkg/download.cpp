3943a8d8162087a5f40936c304d072b11ef9652c
Thomas1664 <46387399+Thomas1664@users.noreply.github.com>
2024-04-02 15:26:52 +0000

    Make common fields reusable

--- src/vcpkg/download.cpp
+++ src/vcpkg/download.cpp
@@ -6,9 +6,61 @@
 
 namespace vcpkg
 {
+    struct GitLikeInfo
+    {
+        std::string repo;
+        std::string ref;
+        std::string sha_512;
+        std::string out_var;
+        std::string file_name;
+        std::vector<std::string> patches;
+    };
+
+    struct GitLikeDeserializer final : Json::IDeserializer<GitLikeInfo>
+    {
+        LocalizedString type_name() const override { return LocalizedString::from_raw("GitLike"); }
+
+        Optional<type> visit_object(Json::Reader& r, const Json::Object& obj) const override
+        {
+            GitLikeInfo result;
+
+            r.required_object_field(type_name(), obj, "repo", result.repo, Json::UntypedStringDeserializer::instance);
+            r.required_object_field(type_name(), obj, "ref", result.ref, Json::UntypedStringDeserializer::instance);
+            r.required_object_field(
+                type_name(), obj, "sha512", result.sha_512, Json::UntypedStringDeserializer::instance);
+            r.required_object_field(
+                type_name(), obj, "out-var", result.out_var, Json::UntypedStringDeserializer::instance);
+
+            r.optional_object_field(obj, "patches", result.patches, Json::IdentifierArrayDeserializer::instance);
+
+            result.file_name = fmt::format("{}-{}.tar.gz", result.repo, result.ref);
+            Strings::inplace_replace_all(result.file_name, '/', '-');
+
+            return result;
+        }
+
+        static const GitLikeDeserializer instance;
+    };
+
+    const GitLikeDeserializer GitLikeDeserializer::instance;
+
     struct GitHubDeserializer final : Json::IDeserializer<DownloadedFile>
     {
         LocalizedString type_name() const override { return LocalizedString::from_raw("GitHub"); }
+
+        View<StringView> valid_fields() const override
+        {
+            static constexpr StringView valid_fields[] = {
+                StringLiteral{"repo"},
+                StringLiteral{"ref"},
+                StringLiteral{"sha512"},
+                StringLiteral{"out-var"},
+                StringLiteral{"host"},
+                StringLiteral{"authorization-token"},
+                StringLiteral{"patches"},
+            };
+            return valid_fields;
+        }
 
         Optional<type> visit_object(Json::Reader& r, const Json::Object& obj) const override
         {
@@ -18,26 +70,32 @@ namespace vcpkg
             result.headers.emplace_back("Accept: application/vnd.github+json");
             result.headers.emplace_back("X-GitHub-Api-Version: 2022-11-28");
 
-            std::string repo;
-            r.required_object_field(type_name(), obj, "repo", repo, Json::UntypedStringDeserializer::instance);
-            std::string ref;
-            r.required_object_field(type_name(), obj, "ref", ref, Json::UntypedStringDeserializer::instance);
-            r.required_object_field(type_name(), obj, "sha512", result.sha_512, Json::UntypedStringDeserializer::instance);
-            r.required_object_field(type_name(), obj, "out-var", result.out_var, Json::UntypedStringDeserializer::instance);
+            auto maybe_git_result = r.visit(obj, GitLikeDeserializer::instance);
+            if (!maybe_git_result.has_value())
+            {
+                return nullopt;
+            }
+
+            auto& git_result = *maybe_git_result.get();
+
             std::string gh_gost = "github.com";
             r.optional_object_field(obj, "host", gh_gost, Json::UntypedStringDeserializer::instance);
             std::string auth_token;
-            if (r.optional_object_field(obj, "authorization-token", auth_token, Json::UntypedStringDeserializer::instance))
+            if (r.optional_object_field(
+                    obj, "authorization-token", auth_token, Json::UntypedStringDeserializer::instance))
             {
                 result.headers.push_back("Authorization: Bearer " + auth_token);
             }
 
             r.optional_object_field(obj, "patches", result.patches, Json::IdentifierArrayDeserializer::instance);
 
-            result.urls.emplace_back(fmt::format("https://api.{}/repos/{}/tarball/{}", gh_gost, repo, ref));
+            result.urls.emplace_back(
+                fmt::format("https://api.{}/repos/{}/tarball/{}", gh_gost, git_result.repo, git_result.ref));
 
-            result.file_name = fmt::format("{}-{}.tar.gz", repo, ref);
-            Strings::inplace_replace_all(result.file_name, '/', '-');
+            result.sha_512 = std::move(git_result.sha_512);
+            result.out_var = std::move(git_result.out_var);
+            result.patches = std::move(git_result.patches);
+            result.file_name = std::move(git_result.file_name);
 
             return result;
         }
@@ -51,6 +109,19 @@ namespace vcpkg
     {
         LocalizedString type_name() const override { return LocalizedString::from_raw("download.json"); }
 
+        View<StringView> valid_fields() const override
+        {
+            static constexpr StringView valid_fields[] = {
+                StringLiteral{"github"},
+                StringLiteral{"gitlab"},
+                StringLiteral{"git"},
+                StringLiteral{"bitbucket"},
+                StringLiteral{"sourceforge"},
+                StringLiteral{"distfile"},
+            };
+            return valid_fields;
+        }
+
         Optional<type> visit_object(Json::Reader& r, const Json::Object& obj) const override
         {
             DownloadedFile result;
@@ -58,6 +129,31 @@ namespace vcpkg
             {
                 r.visit_in_key(*value, "github", result, GitHubDeserializer::instance);
             }
+            else if (auto value = obj.get("gitlab"))
+            {
+                r.visit_in_key(*value, "gitlab", result, GitHubDeserializer::instance);
+            }
+            else if (auto value = obj.get("git"))
+            {
+                r.visit_in_key(*value, "git", result, GitHubDeserializer::instance);
+            }
+            else if (auto value = obj.get("bitbucket"))
+            {
+                r.visit_in_key(*value, "bitbucket", result, GitHubDeserializer::instance);
+            }
+            else if (auto value = obj.get("sourceforge"))
+            {
+                r.visit_in_key(*value, "sourceforge", result, GitHubDeserializer::instance);
+            }
+            else if (auto value = obj.get("distfile"))
+            {
+                r.visit_in_key(*value, "distfile", result, GitHubDeserializer::instance);
+            }
+            else
+            {
+                Checks::unreachable(VCPKG_LINE_INFO);
+            }
+
             return result;
         }
 
@@ -70,8 +166,8 @@ namespace vcpkg
     {
         Json::Reader reader("download.json");
         auto obj = Json::parse_object(str, "download.json");
-        auto res = reader.array_elements(obj.get()->get("files")->array(VCPKG_LINE_INFO), DownloadDeserializer::instance);
+        auto res =
+            reader.array_elements(obj.get()->get("files")->array(VCPKG_LINE_INFO), DownloadDeserializer::instance);
         return *res.get();
     }
 } // namespace vcpkg
-
